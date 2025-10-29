40aa94400caf6f59467000645f3ee14ea46cc46f
Arya Kondur <aryarksub@gmail.com>
2025-10-29 16:01:24 -0700

    Add documentation for validation file

--- src/util/validation_util.py
+++ src/util/validation_util.py
@@ -6,18 +6,60 @@ import rasterio
 output_cubes_dir = os.path.join('output', 'cubes')
 
 def get_layer_range_data():
+    """
+    Get dictionary mapping layers to important information. Keys are layer names. Values are dictionaries that have
+    keys "category", "min", "max", "nan". These represent the category of the layer, min/max values, and a value
+    that is treated as a null value for the layer.
+
+    Returns:
+        dict: Dictionary of layer information.
+    """
     layer_range_df = pd.read_csv('inputData\layer_ranges.csv')
     key = 'layer'
     return layer_range_df.set_index(key).to_dict(orient='index')
 
 def find_landfire_layer_file(abbrev, lf_dir):
+    """
+    Find the layer file corresponding to the given abbreviation in the given directory of LANDFIRE data.
+
+    Args:
+        abbrev (str): Abbreviation of the LANDFIRE layer to search for.
+        lf_dir (str): Path of the LANDFIRE directory in which we should search.
+
+    Returns:
+        str: Name of the corresponding LANDFIRE layer file.
+    """
     for file in os.listdir(lf_dir):
         if abbrev in file and os.path.isfile(os.path.join(lf_dir, file)):
             return file
     return None
 
 def validate_one_fire_data(fid, layer_data_dict=None):
+    """
+    Determine whether the downloaded data for the given fire (fid) is valid. Validation is done by checking that the data
+    for each layer is either within the min/max range defined in layer_data_dict or the corresponding null value (also
+    defined in layer_data_dict).
+
+    Args:
+        fid (str): Fire event ID for which to validate data.
+        layer_data_dict (dict, optional): Dictionary of layer information (see get_layer_range_data() for dictionary
+         format). Defaults to None.
+
+    Raises:
+        NotADirectoryError: Occurs when there is no directory for the given fire (fid) or there is no directory for
+         a certain layer category.
+        FileNotFoundError: Occurs when there is no TIF file for a certain layer.
+
+    Returns:
+        tuple: Tuple of a boolean and a list. The boolean takes a True value if the fire data is valid; False otherwise.
+         The list contains tuples of the form (layer, band, indices). These store the invalid layers, the time band at
+         which the data is invalid, and the indices in the 2D TIF data array that are invalid.
+    """
+
+    # If no layer data dictionary is specified, then obtain it with get_layer_range_data()
     layer_data_dict = layer_data_dict if layer_data_dict is not None else get_layer_range_data()
+
+    # Ensure there is an output folder for the given FID
     fire_folder = os.path.join('output', 'cubes', fid)
     if not os.path.exists(fire_folder):
         raise NotADirectoryError(f'No folder for fire {fid} exists')
@@ -26,19 +68,21 @@ def validate_one_fire_data(fid, layer_data_dict=None):
     evt_found = False
     
     for layer, layer_data in layer_data_dict.items():
+        # Since there are multiple potential EVT layers (LANDFIRE), if we have already found data for one, skip the others
         if 'evt' in layer and evt_found:
             continue
 
         category = layer_data['category']
-
         min_val, max_val, null_val = layer_data['min'], layer_data['max'], layer_data['nan']
         
+        # Ensure there is a folder for the layer's category
         layer_folder = os.path.join(fire_folder, category)
         if not os.path.exists(layer_folder):
             raise NotADirectoryError(f'No folder for layer category {category} and fire {fid} exists')
         
         # LANDFIRE layers are named differently than just "{layer}.tif"
         if category == 'landfire':
+            # Get the TIF file for the LANDFIRE layer
             lf_layer_file = find_landfire_layer_file(layer, layer_folder)
 
             if lf_layer_file is None:
@@ -65,14 +109,18 @@ def validate_one_fire_data(fid, layer_data_dict=None):
         if 'evt' in layer:
             evt_found = True
         
+        # Data validation for each band in TIF
         with rasterio.open(layer_tif) as src:
             for band in range(src.count):
+                # Band counting is 1-indexed
                 data = src.read(band + 1)
+                # Allow null value if specified
                 if null_val:
                     valid_data_mask = ((data >= min_val) & (data <= max_val)) | (data == null_val)
                 else:
                     valid_data_mask = (data >= min_val) & (data <= max_val)
                 
+                # Store the first occurrence of invalid data
                 if not np.all(valid_data_mask):
                     invalid_indices = np.where(~valid_data_mask)
                     invalid_locations = list(zip(invalid_indices[0], invalid_indices[1]))
@@ -83,11 +131,26 @@ def validate_one_fire_data(fid, layer_data_dict=None):
 
 
 def validate_existing_fires(layer_data_dict=None, verbose=False):
+    """
+    Perform data validation for all fires that are stored in the output directory. This is done by simply iterating
+    through the directory and running validate_one_fire_data().
+
+    Args:
+        layer_data_dict (dict, optional): Dictionary of layer information (see get_layer_range_data() for dictionary
+         format). Defaults to None.
+        verbose (bool, optional): True if descriptive messages should be printed; False otherwise. Defaults to False.
+
+    Returns:
+        list: List of tuples of the form (fid, invalid_layers), where fid corresponds to a fire event ID with invalid
+         data and invalid_layers is the list returned by validate_one_fire_data().
+    """
+    # If no layer data dictionary is specified, then obtain it with get_layer_range_data()
     layer_data_dict = layer_data_dict if layer_data_dict is not None else get_layer_range_data()
 
     invalid_fires = []
     processed_count = 0
 
+    # Note that folders are named with the fire event ID (so folder = FID)
     for folder in os.listdir(output_cubes_dir):
         if os.path.isdir(os.path.join(output_cubes_dir, folder)):
             valid_data, invalid_layers = validate_one_fire_data(folder, layer_data_dict)
