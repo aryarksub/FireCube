ed058a5a1671317813f6953f42939c122d56786f
Arya Kondur <aryarksub@gmail.com>
2025-11-01 18:51:57 -0700

    Add documentation for general util

--- src/util/general_util.py
+++ src/util/general_util.py
@@ -43,6 +43,12 @@ var_types = [subdir_type_original, subdir_type_converted, subdir_type_resample]
 data_batches = [subdir_vis, subdir_lrc, subdir_hrc, subdir_fuel_topo, subdir_landfire, subdir_firespread, subdir_frp]
 
 def create_dirs_for_fire(fid):
+    """
+    Create the required directories for the given fire.
+
+    Args:
+        fid (str): Fire event ID.
+    """
     # create folders in temporary (local) directory
     for top_dir in [dir_data, dir_videos]:
         top_path = os.path.join(dir_temp, top_dir)
@@ -70,15 +76,56 @@ def create_dirs_for_fire(fid):
             os.makedirs(batch_path)
 
 def get_era5_nc_filename(fid):
+    """
+    Get name of the given fire's ERA5 NetCDF file.
+
+    Args:
+        fid (str): Fire event ID.
+
+    Returns:
+        str: Path to ERA5 NetCDF file.
+    """
     return os.path.join(dir_temp, dir_data, fid, subdir_era5, f'{fid}_era5_original.nc')
 
 def get_pyr_tar_filename(fid):
+    """
+    Get name of the given fire's Pyregence TAR file.
+
+    Args:
+        fid (str): Fire event ID.
+
+    Returns:
+        str: Path to the Pyregence TAR file.
+    """
     return os.path.join(dir_temp, dir_data, fid, subdir_pyr, f'{fid}_pyr_original.tar')
 
 def get_lf_zip_filename(fid):
+    """
+    Get name of the given fire's LANDFIRE zip file.
+
+    Args:
+        fid (str): Fire event ID.
+
+    Returns:
+        str: Path to the LANDFIRE zip file.
+    """
     return os.path.join(dir_temp, dir_data, fid, subdir_lf, f'{fid}_lf_original.zip')
 
 def get_temp_data_video_filename(fid, var, dir_type='data', data_source='era5', var_type='original'):
+    """
+    Get name of the desiredtemporary  data/video file for the given fire based on the variable, data type, data source, 
+    and variable type.
+
+    Args:
+        fid (str): Fire event ID.
+        var (str): Variable name.
+        dir_type (str, optional): Type of directory where file resides. Defaults to 'data'.
+        data_source (str, optional): Data source name. Defaults to 'era5'.
+        var_type (str, optional): Type of variable (original, converted, resample). Defaults to 'original'.
+
+    Returns:
+        str: Path to the data/video file.
+    """
     assert dir_type in [dir_data, dir_videos]
     assert data_source in data_sources
     assert var_type in var_types
@@ -87,41 +134,83 @@ def get_temp_data_video_filename(fid, var, dir_type='data', data_source='era5', 
     return os.path.join(dir, filename)
 
 def get_out_batch_for_tif(tif):
+    """
+    Get the batch for the given variable TIF file. Batches are used to group variables into similar categories based
+    on their inherent characteristics. A full list of data batches can be found in the data_batches list.
+
+    Args:
+        tif (str): File name of variable TIF file.
+
+    Returns:
+        str: Batch name for the given TIF file.
+    """
     with rasterio.open(tif) as src:
         res_x, res_y = src.transform.a, -src.transform.e
         assert res_x == res_y
     
+    # ERA5 variables have resolution 9000m and are under the "low-resolution climate" batch
     if res_x == 9000 and subdir_era5 in tif:
         return subdir_lrc
+    # Pyregence variables that have resolution 600m are under the "high-resolution climate" batch
     elif res_x == 600 and subdir_pyr in tif:
         return subdir_hrc
+    # Pyregence variables that have resolution 30m are under the "fuel and topography" batch
     elif res_x == 30 and subdir_pyr in tif:
         return subdir_fuel_topo
+    # LANDFIRE variables have their own batch ("landfire")
     elif subdir_lf in tif:
         return subdir_landfire
+    # FEDS variables have their own batch ("fire spread")
     elif subdir_feds in tif:
         return subdir_firespread
+    # FRP variables have their own batch ("frp")
     elif subdir_frp in tif:
         return subdir_frp
+    # All other variables are not supported
     else:
         print(f'Resolution + Var combination for file {tif} is not supported')
 
 def get_output_data_filename(fid, var, batch_dir):
+    """
+    Get the name of the output file name for the given fire, variable, and batch.
+
+    Args:
+        fid (str): Fire event ID.
+        var (str): Variable/Layer name.
+        batch_dir (str): Name of the batch (directory) under which the variable output file should be stored.
+
+    Returns:
+        str: Output variable file name.
+    """
     assert batch_dir in data_batches
     dir = os.path.join(dir_output, dir_cubes, fid, batch_dir)
     filename = f"{var}.{'mp4' if batch_dir == subdir_vis else 'tif'}"
     return os.path.join(dir, filename)
 
 def get_all_var_and_output_tifs_for_fire(fid, exclude=[]):
+    """
+    Get a list of all temporary variable data files and output data files for the given fire. Also, exclude
+    any specified data sources.
+
+    Args:
+        fid (str): Fire event ID.
+        exclude (list, optional): List of data sources to exclude. Defaults to [].
+
+    Returns:
+        tuple: Tuple of two lists. The first list contains paths to the temporary variable data files. The second
+        list contains paths to the final output variable data files.
+    """
     var_tifs = []
     out_tifs = []
     for data_source in data_sources:
         # Do not get tifs for excluded data sources
         if data_source in exclude:
             continue
+        # Get the variables in the data source directory
         data_vars = get_tif_vars_in_dir(
             os.path.join(dir_temp, dir_data, fid, data_source, subdir_type_resample)
         )
+        # Get temp data and output files for each variable
         for data_var in data_vars:
             var_tif = get_temp_data_video_filename(
                 fid, data_var, dir_type=dir_data, data_source=data_source,
@@ -138,21 +227,41 @@ def get_all_var_and_output_tifs_for_fire(fid, exclude=[]):
 
 def to_datetime(date):
     """
-    Converts a numpy datetime64 object to a python datetime object 
-    Input:
-      date - a np.datetime64 object
-    Output:
-      DATE - a python datetime object
+    Converts a numpy datetime64 object to a Python datetime object.
+
+    Args:
+        date (np.datetime64): NumPy datetime64 object
+
+    Returns:
+        datetime.datetime: Equivalent Python datetime object.
     """
     timestamp = ((date - np.datetime64('1970-01-01T00:00:00'))
                  / np.timedelta64(1, 's'))
     return datetime.utcfromtimestamp(timestamp)
 
 def get_fire_start_from_ds(ds):
+    """
+    Get the fire start time from the given dataset. This is just the time furthest in the past.
+
+    Args:
+        ds (xarray.Dataset): Dataset with fire information, sorted by time.
+
+    Returns:
+        datetime.datetime: Datetime object representing fire start time.
+    """
     return to_datetime(ds['valid_time'].values[0])
 
 ## Code to get the extent of a raster
 def get_extent(profile):
+    """
+    Get the spatial extent (bounding box) given a rasterio profile.
+
+    Args:
+        profile (rasterio.Profile): Raster profile dictionary
+
+    Returns:
+        tuple: Tuple representing spatial extent in terms of left, right, bottom, and top coordinates.
+    """
     transform = profile['transform']
     width, height = profile['width'], profile['height']
     left, top = transform * (0, 0)
@@ -160,6 +269,19 @@ def get_extent(profile):
     return (left, right, bottom, top)
 
 def create_animation_plot_from_tif(in_tif, out_file, start_time, mask=False, ignore_small_neg=False):
+    """
+    Create an animation plot of the given TIF file from the given start time. Optionally, do not include zero/null
+    values and/or very small negative values when computing min/max values for plotting.
+
+    Args:
+        in_tif (str): Name of input variable TIF file.
+        out_file (str): Path of where output video should be stored.
+        start_time (datetime.datetime): Datetime object representing start time of the plot/data.
+        mask (bool, optional): True if zero/NaN should be omitted from min/max computation; False otherwise. 
+         Defaults to False.
+        ignore_small_neg (bool, optional): True if small negative values should be omitted from min/max
+         computation; False otherwise. Defaults to False.
+    """
     with rasterio.open(in_tif) as src:
         data = src.read()
         transform = src.transform
@@ -202,11 +324,28 @@ def create_animation_plot_from_tif(in_tif, out_file, start_time, mask=False, ign
     plt.close()
 
 def get_tif_vars_in_dir(dir):
+    """
+    Get a list of all variables represented by TIFs in the given directory.
+
+    Args:
+        dir (pathlib.Path): Path object of directory in which to search.
+
+    Returns:
+        list: List of variables stored in directory.
+    """
     if type(dir) is str:
         dir = Path(dir)
     return [f.stem for f in dir.glob('*.tif')]
 
 def create_multi_animation_from_tifs(in_tifs, out_file, start_time):
+    """
+    Create an animation plot of the given TIF files from the given start time.
+
+    Args:
+        in_tifs (list): Name of input variable TIF files.
+        out_file (str): Path of where output video should be stored.
+        start_time (datetime.datetime): Datetime object representing start time of the plot/data.
+    """
     def get_data_and_extent(tif):
         with rasterio.open(tif) as src:
             extent = get_extent(src.profile)
@@ -219,6 +358,7 @@ def create_multi_animation_from_tifs(in_tifs, out_file, start_time):
     def grid_layout(n, num_cols=4):
         return int(np.ceil(n / num_cols)), int(num_cols)
     
+    # Same type of masking done in create_animation_plot_from_tif()
     def get_vmin_vmax(data, mask=False, ignore_small_neg=False):
         condition = np.ones_like(data, dtype=bool)
         # If masking to ignore nan and zero values
@@ -283,12 +423,32 @@ def create_multi_animation_from_tifs(in_tifs, out_file, start_time):
     plt.close()
 
 def create_multi_animation_for_dir(dir, out_file, start_time):
+    """
+    Create an animation plot of the TIF files in the given directory starting from the given start time.
+
+    Args:
+        dir (str): Name of directory from which TIF files should be used for plotting.
+        out_file (str): Path of where output video should be stored.
+        start_time (datetime.datetime): Datetime object representing start time of the plot/data.
+    """
     tif_files = [
         os.path.join(dir, f) for f in os.listdir(dir) if f.endswith(".tif")
     ]
     create_multi_animation_from_tifs(tif_files, out_file, start_time)
 
 def remove_temp_dir_files(fid, del_dir_types=[], del_data_sources=[], del_var_types=[], remove_intermediate=False, verbose=False):
+    """
+    Remove temporary data files based on the types of directory/data source/variable to delete.
+
+    Args:
+        fid (str): Fire event ID.
+        del_dir_types (list, optional): List of directory types to remove (see dir_types). Defaults to [].
+        del_data_sources (list, optional): List of data sources to remove (see data_sources). Defaults to [].
+        del_var_types (list, optional): List of variable types to remove (see var_types). Defaults to [].
+        remove_intermediate (bool, optional): True if intermediate data files (e.g. ERA NetCDF, Pyregence TAR,
+         LANDFIRE zip) should be removed; False otherwise. Defaults to False.
+        verbose (bool, optional): True if descriptive messages should be printed; False otherwise. Defaults to False.
+    """
     if verbose:
         print(f"Deleting files with directory types {del_dir_types}, data sources {del_data_sources}, variable types {del_var_types} + with{'' if remove_intermediate else 'out'} removing intermediate files")
 
